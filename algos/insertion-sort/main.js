0f0b7d1245e3dffe88cc61eeefdd90143b0b057b
Theophilus Annor <annor_theophilus@hotmail.com>
2024-12-04 10:28:44 +0000

    replace timestamp with date

--- algos/insertion-sort/main.js
+++ algos/insertion-sort/main.js
@@ -1,18 +1,46 @@
+const n = 1000
 
+function sortPrimitiveList() {
+  const list = Array.from({ length: 100_000_000}).map(()=>{
+    return Number((Math.random() * 10000).toFixed(2))
+  })
+  
 
-const list = [20, 0, 5, 2, 1, 6, 8]
+ console.time("insertion_sort")
+  for (let i = 1; i < list.length; i++) {
+    let j = i;
+    const currentValue = list[i];
 
-for (let i = 1; i < list.length; i++) {
-
-    let j = i
-    const currentValue = list[i]
-
-    while (j > 0 && currentValue < list[j -1]) {
-        list[j] = list[j - 1]
-        list[j - 1] = currentValue
-        j -= 1
+    while (j > 0 && currentValue < list[j - 1]) {
+      list[j] = list[j - 1];
+      list[j - 1] = currentValue;
+      j -= 1;
     }
-
+  }
+  console.timeEnd("insertion_sort")
+  console.log(list);
 }
 
-console.log(list)
+function sortArrayObjects() {
+  const list = [
+    { name: "David", age: 27 },
+    { name: "John", age: 25 },
+    { name: "Alice", age: 30 },
+    { name: "Bob", age: 28 },
+    { name: "Charlie", age: 22 },
+  ];
+
+  for (let i = 0; i < list.length; i++) {
+    let j = i
+    const current = list[i]
+    while (j > 0 &&  current.age < list[j - 1].age) {
+        list[j] = list[j - 1]
+        list[j - 1] = current
+        j -= 1
+    }
+    
+  }
+  console.log(list);
+}
+
+sortPrimitiveList();
